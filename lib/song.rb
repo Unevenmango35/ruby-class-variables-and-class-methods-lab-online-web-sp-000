2e6bbe33926507c87afdfa5df21d1afecf36a06b
Rosie Choi <rosiechoimanso@gmail.com>
2020-10-06 03:56:58 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre
+  attr_reader :name, :artist, :genre
+  
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+ @@genres << @genre
+ @@artists << @artist
+end
+
+def self.count
+  @@count
+end
+
+def self.genres
+  @@genres = @@genres.uniq
+end
+
+def self.artists
+  @@artists = @@artists.uniq
+end
+
+
+def self.genre_count
+  self.genres.map {|i| [i,@@genres.count(i)]}.to_h
+end
+
+
+def self.artist_count
+  self.artists.map {|i| [i,@@artists.count(i)]}.to_h
+end
+
+
+  
+end
+
